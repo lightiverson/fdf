c819bce7dd32895b29852ea003b82b607552d4e0
Kawish Gajadien <kawishg@outlook.com>
2021-12-03 14:35:02 +0100

    Add hardcoded matrix

--- get_map.c
+++ get_map.c
@@ -68,7 +68,7 @@ unsigned int	get_columns(const char *map_name)
 	int				return_get_next_line;
 	char			*line;
 	unsigned int	columns;
-	char			**points;
+	char			**vertex;
 
 	map_fd = get_map_fd(map_name);
 	columns = 0;
@@ -79,10 +79,10 @@ unsigned int	get_columns(const char *map_name)
 		printf("Error: get_next_line returned -1\n");
 		exit(EXIT_FAILURE);
 	}
-	points = ft_split(line, ' ');
-	while (points[columns] != NULL)
+	vertex = ft_split(line, ' ');
+	while (vertex[columns] != NULL)
 		columns++;
-	free_splitted_array(points);
+	free_splitted_array(vertex);
 	close(map_fd);
 	return (columns);
 }
@@ -100,27 +100,124 @@ int *calloc_matrix(unsigned int rows, unsigned int columns)
 	return (matrix);
 }
 
+/*
+dit wil ik uiteindelijke krijgen:
+	   x  y
+matrix[0][0] = 0
+matrix[1][0] = 0
+matrix[2][0] = 1
+matrix[3][0] = 2
+matrix[4][0] = 3
+matrix[5][0] = 4
+matrix[6][0] = 5
+matrix[7][0] = 6
+matrix[8][0] = 7
+matrix[9][0] = 8
+matrix[10][0] = 9
+ */
+
 // int main(int argc, char const *argv[])
 // {
 // 	unsigned int	rows;
 // 	unsigned int	columns;
 // 	int				*matrix;
 
-// 	rows = get_rows("./test_maps/elem-col.fdf");
+// 	rows = get_rows("./test_maps/basictest.fdf");
 // 	printf("rows = %d\n", rows);
 
-// 	columns = get_columns("./test_maps/elem-col.fdf");
+// 	columns = get_columns("./test_maps/basictest.fdf");
 // 	printf("columns = %d\n", columns);
 
+// 	matrix = calloc_matrix(rows, columns);
+
+// 	int				map_fd;
+// 	int				return_get_next_line;
+// 	char			*line;
+// 	char			**vertex;
+
+// 	map_fd = get_map_fd("./test_maps/basictest.fdf");
+// 	return_get_next_line = 1;
+// 	while (return_get_next_line == 1)
+// 	{
+// 		return_get_next_line = get_next_line(map_fd, &line);
+// 		if (return_get_next_line == -1)
+// 		{
+// 			free(line);
+// 			printf("Error: get_next_line returned -1\n");
+// 			exit(EXIT_FAILURE);
+// 		}
+// 		if (return_get_next_line == 1)
+// 		{
+// 			printf("|%s|\n", line); // |0 0 1 2 3 4 5 6 7 8 9|
+			
+// 		}
+// 		free(line);
+// 	}
+// 	close(map_fd);
 // 	return (0);
 // }
+
+
+typedef struct	s_data {
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+} t_data;
+
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
+}
 
 int	main(void)
 {
 	void	*mlx;
 	void	*mlx_win;
+	t_data	img;
+	int matrix[9][11] = 
+	{
+		{0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9},
+		{0, 0, 0, 1, 2, 3, 4, 5, 6, 7, 8},
+		{0, 0, 0, 0, 1, 2, 3, 4, 5, 6, 7},
+		{0, 0, 0, 0, 0, 1, 2, 3, 4, 5, 6},
+		{0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 5},
+		{0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4},
+		{0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}
+	};
 
-	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
-	mlx_loop(mlx);
+	int i = 0;
+	int j = 0;
+	while (i < 9)
+	{
+		while (j < 11)
+		{
+			printf("%d ", matrix[i][j]);
+			j++;
+		}
+		printf("\n");
+		j = 0;
+		i++;
+	}
+	
+
+	// mlx = mlx_init();
+	// mlx_win = mlx_new_window(mlx, 1280, 720, "Hello world!");
+	// img.img = mlx_new_image(mlx, 1280, 720);
+	// img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
+	// 								&img.endian);
+	// my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
+	// my_mlx_pixel_put(&img, 6, 6, 0x00FFFFFF);
+	// my_mlx_pixel_put(&img, 7, 7, 0x00FFFFFF);
+	// my_mlx_pixel_put(&img, 8, 8, 0x00FFFFFF);
+	// my_mlx_pixel_put(&img, 9, 9, 0x00FFFFFF);
+	// my_mlx_pixel_put(&img, 10, 10, 0x00FF0000);
+	// mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+	// mlx_loop(mlx);
 }
