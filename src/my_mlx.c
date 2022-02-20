47fd1a3b195ba93f76feb1277274e01df1240f1a
Kawish Gajadien <kawishg@outlook.com>
2022-02-20 09:39:55 +0100

    Norm proof my_mlx

--- src/my_mlx.c
+++ src/my_mlx.c
@@ -5,7 +5,7 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 	char	*dst;
 
 	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
-	*(unsigned int*)dst = color;
+	*(unsigned int *)dst = color;
 }
 
 void	create_mlx_image(t_data *img)
@@ -13,5 +13,6 @@ void	create_mlx_image(t_data *img)
 	img->mlx = mlx_init();
 	img->mlx_win = mlx_new_window(img->mlx, WINDOW_WIDTH, WINDOW_HEIGHT, "FDF");
 	img->img = mlx_new_image(img->mlx, WINDOW_WIDTH, WINDOW_HEIGHT);
-	img->addr = mlx_get_data_addr(img->img, &img->bits_per_pixel, &img->line_length, &img->endian);
+	img->addr = mlx_get_data_addr(img->img, &img->bits_per_pixel,
+			&img->line_length, &img->endian);
 }
--- src/my_mlx.h
+++ src/my_mlx.h
@@ -1,14 +1,14 @@
 #ifndef MY_MLX_H
-#define MY_MLX_H
+# define MY_MLX_H
 
-#include "./mlx/mlx.h"
+# include "./mlx/mlx.h"
 
-#define WINDOW_WIDTH 1000
-#define WINDOW_HEIGHT 1000
-#define MAP_WIDTH WINDOW_WIDTH * 0.8 
-#define MAP_HEIGHT WINDOW_HEIGHT * 0.8
+# define WINDOW_WIDTH 1000
+# define WINDOW_HEIGHT 1000
+# define MAP_WIDTH WINDOW_WIDTH * 0.8 
+# define MAP_HEIGHT WINDOW_HEIGHT * 0.8
 
-typedef struct	s_data {
+typedef struct s_data {
 	void	*img;
 	char	*addr;
 	int		bits_per_pixel;
@@ -16,7 +16,7 @@ typedef struct	s_data {
 	int		endian;
 	void	*mlx;
 	void	*mlx_win;
-} t_data;
+}	t_data;
 
 void	my_mlx_pixel_put(t_data *data, int x, int y, int color);
 void	create_mlx_image(t_data *img);
