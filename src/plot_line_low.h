bc25ff4d09cf40ab567ea4411a71ed645b03d3bc
Kawish Gajadien <kawishg@outlook.com>
2022-03-10 15:47:01 +0100

    WIP van school

---
+++ src/plot_line_low.h
@@ -0,0 +1,17 @@
+#ifndef PLOT_LINE_LOW_H
+# define PLOT_LINE_LOW_H
+
+# include "my_mlx.h"
+# include <stdio.h>
+
+typedef struct s_low_data
+{
+	int dx;
+	int dy;
+	int yi;
+	int D;
+	int y;
+	int x0_dup;
+}	t_low_data;
+
+#endif // PLOT_LINE_LOW_H
