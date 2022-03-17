b3b46983b8cf1ed44019cda6273af7442b449151
Kawish Gajadien <kawishg@outlook.com>
2022-03-17 10:04:57 +0100

    Norm proof edges

--- src/edges.c
+++ src/edges.c
@@ -1,28 +1,28 @@
 #include "edges.h"
 
-t_edge *calloc_edges(unsigned int number_of_edges)
+t_edge	*calloc_edges(unsigned int number_of_edges)
 {
-    t_edge *edges;
+	t_edge	*edges;
 
-    edges = calloc(number_of_edges, sizeof(*edges));
-    if (edges == NULL)
-    {
-        perror("Error: malloc() ");
-        exit(EXIT_FAILURE);
-    }
-    return (edges);
+	edges = calloc(number_of_edges, sizeof(*edges));
+	if (edges == NULL)
+	{
+		perror("Error: malloc() ");
+		exit(EXIT_FAILURE);
+	}
+	return (edges);
 }
 
-void populate_edges(t_fdf_data *fdf_data)
+void	populate_edges(t_fdf_data *fdf_data)
 {
-    unsigned int i;
+	unsigned int	i;
 
-    i = 0;
-    fdf_data->edges = calloc_edges(fdf_data->number_of_edges);
-    while (i < fdf_data->number_of_edges)
-    {
-        fdf_data->edges[i].start_node = fdf_data->nodes + i;
-        fdf_data->edges[i].end_node = fdf_data->nodes + i + 1;
-        i++;
-    }
+	i = 0;
+	fdf_data->edges = calloc_edges(fdf_data->number_of_edges);
+	while (i < fdf_data->number_of_edges)
+	{
+		fdf_data->edges[i].start_node = fdf_data->nodes + i;
+		fdf_data->edges[i].end_node = fdf_data->nodes + i + 1;
+		i++;
+	}
 }
