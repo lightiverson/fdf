7aaa81161ecd549d70a54381cf055a4b6dc9770b
Kawish Gajadien <kawishg@outlook.com>
2022-04-10 17:09:15 +0200

    Zorg dat raster horizontaal centered nadat z en x geroteerd zijn

--- src/translate_raster.c
+++ src/translate_raster.c
@@ -6,7 +6,7 @@
 /*   By: kawish <kawish@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/04/09 14:20:42 by kawish        #+#    #+#                 */
-/*   Updated: 2022/04/10 15:06:34 by kawish        ########   odam.nl         */
+/*   Updated: 2022/04/10 17:01:28 by kawish        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,8 +25,6 @@ int	get_smallest_x(t_fdf_data *fdf_data)
 			smallest_x = fdf_data->nodes[i].x;
 		i++;
 	}
-
-	printf("smallest_x = %i\n", smallest_x);
 	return (smallest_x);
 }
 
@@ -43,9 +41,39 @@ int	get_largest_x(t_fdf_data *fdf_data)
 			largest_x = fdf_data->nodes[i].x;
 		i++;
 	}
-
-	printf("largest_x = %i\n", largest_x);
 	return (largest_x);
+}
+
+int	get_smallest_y(t_fdf_data *fdf_data)
+{
+	unsigned int	i;
+	int				smallest_y;
+
+	i = 1;
+	smallest_y = fdf_data->nodes[0].y;
+	while (i < fdf_data->number_of_nodes)
+	{
+		if (fdf_data->nodes[i].y < smallest_y)
+			smallest_y = fdf_data->nodes[i].y;
+		i++;
+	}
+	return (smallest_y);
+}
+
+int	get_largest_y(t_fdf_data *fdf_data)
+{
+	unsigned int	i;
+	int				largest_y;
+
+	i = 1;
+	largest_y = fdf_data->nodes[0].y;
+	while (i < fdf_data->number_of_nodes)
+	{
+		if (fdf_data->nodes[i].y > largest_y)
+			largest_y = fdf_data->nodes[i].y;
+		i++;
+	}
+	return (largest_y);
 }
 
 unsigned int	calc_raster_width(t_fdf_data *fdf_data)
@@ -57,8 +85,6 @@ unsigned int	calc_raster_width(t_fdf_data *fdf_data)
 	left_border = get_smallest_x(fdf_data);
 	right_border = get_largest_x(fdf_data);
 	raster_width = right_border - left_border;
-
-	printf("raster_width = %i\n", raster_width);
 	return (raster_width);
 }
 
@@ -68,8 +94,8 @@ unsigned int	calc_raster_height(t_fdf_data *fdf_data)
 	int	bottom_border;
 	int	raster_height;
 
-	top_border = fdf_data->nodes[0].y;
-	bottom_border = fdf_data->nodes[fdf_data->number_of_nodes - 1].y;
+	top_border = get_smallest_y(fdf_data);
+	bottom_border = get_largest_y(fdf_data);
 	raster_height = bottom_border - top_border;
 	return (raster_height);
 }
@@ -78,18 +104,19 @@ void	translate_raster(t_fdf_data *fdf_data)
 {
 	unsigned int	raster_width;
 	unsigned int	raster_height;
+	int				smallest_x;
 	unsigned int	i;
 
 	raster_width = calc_raster_width(fdf_data);
 	raster_height = calc_raster_height(fdf_data);
-
-	int smallest_x = get_smallest_x(fdf_data);
-
+	smallest_x = get_smallest_x(fdf_data);
 	i = 0;
 	while (i < fdf_data->number_of_nodes)
 	{
-		fdf_data->nodes[i].x = fdf_data->nodes[i].x + 500 + abs(smallest_x) - (raster_width / 2);
-		fdf_data->nodes[i].y = fdf_data->nodes[i].y;
+		fdf_data->nodes[i].x = fdf_data->nodes[i].x
+			+ (SCREEN_W / 2) + abs(smallest_x) - (raster_width / 2);
+		fdf_data->nodes[i].y = fdf_data->nodes[i].y
+			+ (SCREEN_H / 2) - (raster_height / 2);
 		i++;
 	}
 }
